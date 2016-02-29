fd1a8c1c5885bbfcb35c5bc20c6ca125581dc6df
Massimo Menichinelli <info@openp2pdesign.org>
2016-02-29 10:12:03 +0100

    Create a style folder where to keep the development of the style

---
+++ style/js/particle.js
@@ -0,0 +1,115 @@
+particlesJS("particles-js", {
+    "particles": {
+        "number": {
+            "value": 120,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": ["#7DB478", "#EEAA56", "#F16456", "#3E6684"]
+        },
+        "shape": {
+            "type": ["circle", "triangle", "polygon", "edge"],
+            "stroke": {
+                "width": 0,
+                "color": "#bebebe"
+            },
+            "polygon": {
+                "nb_sides": 6
+            },
+            "image": {
+                "src": "img/github.svg",
+                "width": 100,
+                "height": 100
+            }
+        },
+        "opacity": {
+            "value": 0.4,
+            "random": false,
+            "anim": {
+                "enable": false,
+                "speed": 1,
+                "opacity_min": 0,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 6,
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 40,
+                "size_min": 0.1,
+                "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#bebebe",
+            "opacity": 0.4,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 4,
+            "direction": "none",
+            "random": true,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+            }
+        }
+    },
+    "interactivity": {
+        "detect_on": "window",
+        "events": {
+            "onhover": {
+                "enable": false,
+                "mode": "bubble"
+            },
+            "onresize": {
+                "enable": true,
+                "density_auto": true,
+                "density_area": 400 // nb_particles = particles.nb * (canvas width *  canvas height / 1000) / density_area
+            },
+            "onclick": {
+                "enable": false,
+                "mode": "push"
+            },
+            "resize": true
+        },
+        "modes": {
+            "grab": {
+                "distance": 400,
+                "line_linked": {
+                    "opacity": 1
+                }
+            },
+            "bubble": {
+                "distance": 400,
+                "size": 11.988011988011989,
+                "duration": 2.4775224775224776,
+                "opacity": 0.4075924075924076,
+                "speed": 3
+            },
+            "repulse": {
+                "distance": 200,
+                "duration": 0.4
+            },
+            "push": {
+                "particles_nb": 4
+            },
+            "remove": {
+                "particles_nb": 2
+            }
+        }
+    },
+    "retina_detect": false
+});
