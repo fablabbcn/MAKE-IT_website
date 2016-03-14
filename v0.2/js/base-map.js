9e842d4e5e490916a127ddfa7729dfcf2df26983
Massimo Menichinelli <info@openp2pdesign.org>
2016-03-14 22:33:05 +0100

    Add libraries as Bower components, add Google Calendar to Maps pages

---
+++ v0.2/js/base-map.js
@@ -0,0 +1,14 @@
+// Set view of Leaflet map based on screen size
+var Stamen_TonerLite = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: 'abcd',
+    minZoom: 0,
+    maxZoom: 20,
+    ext: 'png'
+});
+var map = L.map('map', {
+    center: [48, -3],
+    zoom: 5,
+    zoomControl: true
+});
+var defaultLayer = Stamen_TonerLite.addTo(map);
